a854e0aa6d5aa3e773786dff35c3a19978f14c27
Natnatee <natnatee_mond@hotmail.com>
2024-06-10 10:35:58 +0700

    second

--- src/components/page/Owner.jsx
+++ src/components/page/Owner.jsx
@@ -1,11 +1,11 @@
 
-
+import logo from "../../assets/profile.jpg";
 function PageOne() {
 
   return (
     <div className="flex flex-col ">
       <div className="text-2xl font-extrabold text-center">Mond - 18_Natnatee (Mond/มอนด์)</div>
-      <div className="flex justify-center"><img src="src\assets\profile.jpg" className=" h-96 w-fit"/></div>
+      <div className="flex justify-center"><img src={logo} className=" h-96 w-fit"/></div>
       <div className="text-2xl font-extrabold text-center"> Owner </div>
       <div className="whitespace-pre-line text-center px-14"> Hello everyone this web is assessment web for practice how to use React and tools such as  
         Router, Module Component, UseState and other function in Java Script such as .map .filter ternary hope yor are enjoy my web 
